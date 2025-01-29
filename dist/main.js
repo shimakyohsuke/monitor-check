6393c2c4c9fc208942c43ae1f3513cd76ff2503c
shimakyohsuke <shimakyohsuke@gmail.com>
2025-01-30 03:28:33 +0900

    いろいろ追加

--- dist/main.js
+++ dist/main.js
@@ -26,4 +26,22 @@ document.addEventListener('DOMContentLoaded', () => {
       document.getElementById('diagonal-line-btn').textContent = '非表示';
     }
   });
+
+  document.getElementById('line-btn').addEventListener('click', function () {
+    document.querySelector('.container').classList.toggle('is-line-hide');
+    if (document.querySelector('.container').classList.contains('is-line-hide')) {
+      document.getElementById('line-btn').textContent = '表示';
+    } else {
+      document.getElementById('line-btn').textContent = '非表示';
+    }
+  });
+
+  document.getElementById('controller-toggle-btn').addEventListener('click', function () {
+    document.querySelector('.controller').classList.toggle('is-controller-hide');
+    if (document.querySelector('.controller').classList.contains('is-line-hide')) {
+      document.getElementById('controller-toggle-btn').textContent = '表示';
+    } else {
+      document.getElementById('controller-toggle-btn').textContent = '非表示';
+    }
+  });
 });
